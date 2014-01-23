131c5ddaec3da5db675c61e809369f02952cd951
Erin Hoffman <hoffm386@msu.edu>
2014-01-22 23:43:12 -0500

    Added Homework 2 Changes
    Refactored server.py into multiple functions
    Added different responses for /, /content, /file, /image GET requests,
    and POST requests
    
    Modified test_server.py to test for different responses and GET vs POST

--- server.py
+++ server.py
@@ -3,24 +3,82 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def handle_get_request(conn, path):
+    conn.send("HTTP/1.0 200 OK\r\n")
+    if (path == "/"):
+        conn.send("Content-type: text/html\r\n\r\n")
+        body = """
+        <h1>Hello, world.</h1>This is hoffm386's Web server.
+        <ul>
+          <li><a href='/content'>Content</a></li>
+          <li><a href='/file'>File</a></li>
+          <li><a href='/image'>Image</a></li>
+        </ul>
+        """
+        conn.send(body)
+    elif (path == "/content"):
+        conn.send("Content-type: text/html\r\n\r\n")
+        body = """
+        <h1>Content</h1>
+        This page will contain "content"
+        """
+        conn.send(body)
+    elif (path == "/file"):
+        # once there is actual content here, there might be a type of
+        # application/pdf instead of text/html
+        conn.send("Content-type: text/html\r\n\r\n")
+        body = """
+        <h1>File</h1>
+        This page will contain "file"
+        """
+        conn.send(body)
+    elif (path == "/image"):
+        # once there is actual content here, there might be a type of
+        # image/jpeg or image/png
+        conn.send("Content-type: text/html\r\n\r\n")
+        body = """
+        <h1>Image</h1>
+        This page will contain an "image"
+        """
+        conn.send(body)
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+def handle_post_request(conn):
+    conn.send("HTTP/1.0 200 OK\r\n")
+    conn.send("Content-type: text/html\r\n\r\n")
+    body = """
+    <h1>Post Request</h1>
+    This is not actually what a post request will do, but I have received
+    a post request
+    """
+    conn.send(body)
 
-s.listen(5)                 # Now wait for client connection.
+def handle_connection(conn):
+    request = conn.recv(1000)
+    request_type = request.split(" ")[0]
+    if (request_type == "GET"):
+        path = request.split(" ")[1]
+        handle_get_request(conn, path)
+    elif (request_type == "POST"):
+        handle_post_request(conn)
+    conn.close()
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-    line_1 = "HTTP/1.0 200 OK\r\n"
-    line_2 = "Content-Type:  text/html\r\n\r\n"
-    page_content = "<h1>Hello, world</h1> this is hoffm386's web server."
-    message = line_1 + line_2 + page_content
-    c.send(message)
-    c.close()
+def main():
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
+
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+
+    s.listen(5)                 # Now wait for client connection.
+
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        # Establish connection with client.    
+        c, (client_host, client_port) = s.accept()
+        print 'Got connection from', client_host, client_port
+        handle_connection(c)
+
+if __name__ == '__main__':
+    main()
--- test_server.py
+++ test_server.py
@@ -27,14 +27,80 @@ class FakeConnection(object):
 
 # Test a basic GET call.
 
-def test_handle_connection():
+def test_handle_connection_slash():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is ctb\'s Web server.'
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+        <h1>Hello, world.</h1>This is hoffm386's Web server.
+        <ul>
+          <li><a href='/content'>Content</a></li>
+          <li><a href='/file'>File</a></li>
+          <li><a href='/image'>Image</a></li>
+        </ul>
+        """
+    expected_return = header + body
 
     server.handle_connection(conn)
 
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+        <h1>Content</h1>
+        This page will contain "content"
+        """
+    expected_return = header + body
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_file():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+        <h1>File</h1>
+        This page will contain "file"
+        """
+    expected_return = header + body
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_image():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+        <h1>Image</h1>
+        This page will contain an "image"
+        """
+    expected_return = header + body
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_post():
+    conn = FakeConnection("POST / HTTP/1.1\r\n\r\n")
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+    <h1>Post Request</h1>
+    This is not actually what a post request will do, but I have received
+    a post request
+    """
+    expected_return = header + body
+    server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
