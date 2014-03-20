29cafc4d757c87e1d7026ece230729c1e8917445
xavier.hollis <durandh1@msu.edu>
2014-03-19 20:42:59 -0400

    should really test these things

--- imageapp/root.py
+++ imageapp/root.py
@@ -12,12 +12,12 @@ class RootDirectory(Directory):
     def index(self):
         return html.render('index.html')
 
-    @export(name='jquery')
-    def jquery(self):
+	@export(name='jquery')
+	def jquery(self):
 		dirname = os.path.dirname(__file__)
 		dirname = os.path.join(dirname,"imageapp")
 		jquery_path = os.path.join(dirname,'jquery-1.3.2.min.js')
-        return open(jquery_path).read()
+		return open(jquery_path).read()
 
     @export(name='upload')
     def upload(self):
