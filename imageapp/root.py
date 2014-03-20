913f6ccf6f5a975eb0b83a89d0e9c88e10cca506
xavier.hollis <durandh1@msu.edu>
2014-03-19 20:46:44 -0400

    one more time

--- imageapp/root.py
+++ imageapp/root.py
@@ -6,22 +6,23 @@ import os.path
 from . import html, image
 
 class RootDirectory(Directory):
-    _q_exports = []
+	_q_exports = []
 
-    @export(name='')                    # this makes it public.
-    def index(self):
-        return html.render('index.html')
+	@export(name='')                    # this makes it public.
+	def index(self):
+		return html.render('index.html')
 
 	@export(name='jquery')
 	def jquery(self):
 		dirname = os.path.dirname(__file__)
-		dirname = os.path.join(dirname,"imageapp")
+		dirname = os.path.join(dirname,"")
 		jquery_path = os.path.join(dirname,'jquery-1.3.2.min.js')
+		print jquery_path
 		return open(jquery_path).read()
 
-    @export(name='upload')
-    def upload(self):
-        return html.render('upload.html')
+	@export(name='upload')
+	def upload(self):
+		return html.render('upload.html')
 
 	@export(name='upload_receive')
 	def upload_receive(self):
@@ -40,51 +41,51 @@ class RootDirectory(Directory):
 		the_file.close()
 		return quixote.redirect('./')
 		
-    @export(name='upload2')
-    def upload2(self):
-        return html.render('upload2.html')
+	@export(name='upload2')
+	def upload2(self):
+		return html.render('upload2.html')
 
-    @export(name='upload2_receive')
-    def upload2_receive(self):
-        request = quixote.get_request()
-        print request.form.keys()
+	@export(name='upload2_receive')
+	def upload2_receive(self):
+		request = quixote.get_request()
+		print request.form.keys()
 
-        the_file = request.form['file']
-        print dir(the_file)
-        print 'received file with name:', the_file.base_filename
-        data = the_file.read(int(1e9))
+		the_file = request.form['file']
+		print dir(the_file)
+		print 'received file with name:', the_file.base_filename
+		data = the_file.read(int(1e9))
 
-        image.add_image(data)
+		image.add_image(data)
 
-        return html.render('upload2_received.html')
+		return html.render('upload2_received.html')
 
-    @export(name='upload_ajax')
-    def upload_ajax(self):
-        return html.render('upload_ajax.html')
+	@export(name='upload_ajax')
+	def upload_ajax(self):
+		return html.render('upload_ajax.html')
 
-    @export(name='upload_ajax_receive')
-    def upload_ajax_receive(self):
-        request = quixote.get_request()
-        print request.form.keys()
+	@export(name='upload_ajax_receive')
+	def upload_ajax_receive(self):
+		request = quixote.get_request()
+		print request.form.keys()
 
-        the_file = request.form['file']
-        print dir(the_file)
-        print 'received file with name:', the_file.base_filename
-        data = the_file.fp.read()
+		the_file = request.form['file']
+		print dir(the_file)
+		print 'received file with name:', the_file.base_filename
+		data = the_file.fp.read()
 
-        image.add_image(data)
+		image.add_image(data)
 
-        response = quixote.get_response()
-        response.set_content_type('image/png')
-        return image.get_latest_image()
+		response = quixote.get_response()
+		response.set_content_type('image/png')
+		return image.get_latest_image()
 
-    @export(name='image')
-    def image(self):
-        return html.render('image.html')
+	@export(name='image')
+	def image(self):
+		return html.render('image.html')
 
-    @export(name='image_raw')
-    def image_raw(self):
-        response = quixote.get_response()
-        response.set_content_type('image/png')
-        img = image.get_latest_image()
-        return img
+	@export(name='image_raw')
+	def image_raw(self):
+		response = quixote.get_response()
+		response.set_content_type('image/png')
+		img = image.get_latest_image()
+		return img
